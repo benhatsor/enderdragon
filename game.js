ee4facfcb695575b3fc0b3f9f7ec0618484a560e
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-02-28 17:48:35 +0200

    Update game.js

--- game.js
+++ game.js
@@ -515,8 +515,8 @@ function gameloop() {
       let blockUnder = blockList[blockData[-Math.round(player.pos.x)][-Math.round(player.pos.z)][Math.round(player.pos.y)-1]];
       if (flying && sneaking && blockUnder.id != 0) {
         verticalSpeed = 0;
-        sneaking = true;
         disableFlying();
+        sneak();
       }
       
       if (verticalSpeed <= 0 && clipSneak) {
