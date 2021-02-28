4d74198534bdb95940d4085d9518bc6b91a22efa
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-02-28 16:03:27 +0200

    Update game.js

--- game.js
+++ game.js
@@ -650,13 +650,13 @@ document.onkeydown = function(e) {
 function animateSneak(direction) {
   if (direction = 'forward') {
     if (player.height <= 1.1) {
-      player.height -= 0.05;
+      player.height += 0.05;
       requestAnimationFrame(() => { animateSneak('forward') });
     }
   }
   if (direction = 'backward') {
     if (player.height >= 1.3) {
-      player.height += 0.05;
+      player.height -= 0.05;
       requestAnimationFrame(() => { animateSneak('backward') });
     }
   }
