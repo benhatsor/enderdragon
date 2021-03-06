93fbbb253d50c92222ae2b29ae43fb9b81b07063
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-03-06 12:44:02 +0200

    Update game.js

--- game.js
+++ game.js
@@ -577,7 +577,7 @@ function checkFocus() {
         blockY = newFocusBlock.y,
         blockZ = newFocusBlock.z,
         blockDistance = Math.sqrt(Math.pow(playerX - blockX, 2) + Math.pow(playerY - blockY, 2) + Math.pow(playerZ - blockZ, 2)),
-        blockInRadius = blockDistance > radius;
+        blockInRadius = blockDistance < radius;
     
     if (newFocusBlock == document.body || !blockInRadius) focusBlock = null;
     else {
