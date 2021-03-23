620ba3217c14bc13e87ec07f7447ca3fe98fe46b
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-03-23 16:25:46 +0200

    Update game.js

--- game.js
+++ game.js
@@ -545,7 +545,7 @@ function gameloop() {
       }
       
       // if speed is lower than min velocity, reset
-      if (verticalSpeed < -verticalVelocity) {
+      if (verticalSpeed < 1000) {
         verticalSpeed = 0;
         
         // if clipping edge of block while sneaking, reset
